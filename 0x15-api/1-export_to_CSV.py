a20d78a471305b464bd0fb75e34fefb09062bb70
Jerethink <jerethink@gmail.com>
2023-04-26 02:27:44 +0000

    push

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+'''
+Exports data to CSV
+'''
+import csv
+import os
 import requests
 import sys
 
+
+def main(argv):
+    """ function That export csv"""
+    emp = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                       .format(sys.argv[1]))
+    name = emp.json().get('username')
+    tasks = requests.get('https://jsonplaceholder.typicode.com/todos')
+    tasks = tasks.json()
+    filename = sys.argv[1] + ".csv"
+    with open(filename, 'w') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL, quotechar='"')
+        for task in tasks:
+            if task['userId'] == int(sys.argv[1]):
+                task['name'] = name
+                writer.writerow([task['userId'], task['name'],
+                                 task['completed'], task['title']])
+
+
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
-
+    import sys
+    main(sys.argv)
